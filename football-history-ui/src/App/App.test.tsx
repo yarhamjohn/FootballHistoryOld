d1c6e6baa1a10ed3ec40a6fe06bc893feb754078
yarhamjohn <yarhamjohn@gmail.com>
2021-07-23 16:22:46 +0100

    add testing

--- football-history-ui/src/App/App.test.tsx
+++ football-history-ui/src/App/App.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import reduxStore from "../reduxStore";
+import { Provider } from "react-redux";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
+  );
+  const linkElement = getByText("History of the English Football League");
   expect(linkElement).toBeInTheDocument();
 });
